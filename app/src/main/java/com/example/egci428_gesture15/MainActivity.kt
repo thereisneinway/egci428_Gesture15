ffd1615f0e681e82f712a69d5e5f79101558fa08
Earth Openbox <earthopenbox@gmail.com>
2023-06-06 14:47:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/egci428_gesture15/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.egci428_gesture15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.TextView
+import androidx.core.view.GestureDetectorCompat
+
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    private val MIN_SWIPE_DISTANCE_X = 100
+    private val MAX_SWIPE_DISTANCE_X = 1000
+
+    private val MIN_SWIPE_DISTANCE_Y = 100
+    private val MAX_SWIPE_DISTANCE_Y = 1000
+    private var statusMsg: TextView? = null
+    private var gestureDetector: GestureDetectorCompat? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        statusMsg = findViewById(R.id.statusMsg)
+        this.gestureDetector = GestureDetectorCompat(this, this)
+        gestureDetector!!.setOnDoubleTapListener(this)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        this.gestureDetector!!.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        statusMsg!!.text = "onDown"
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+        statusMsg!!.text = "onShowPress"
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+        statusMsg!!.text = "onSingleTapUp"
+        return true
+    }
+
+    override fun onScroll(event1: MotionEvent, event2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+        statusMsg!!.text = "onScroll"
+        return true
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+        statusMsg!!.text = "onLongPress"
+    }
+
+    override fun onFling(event1: MotionEvent, event2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        val deltaX = event1.x - event2.x
+        val deltaY = event1.y - event2.y
+
+        val deltaX_Abs = Math.abs(deltaX)
+        val deltaY_Abs = Math.abs(deltaY)
+
+        if((deltaX_Abs >= MIN_SWIPE_DISTANCE_X) && (deltaX_Abs<=MAX_SWIPE_DISTANCE_X)){
+            if(deltaX>0){
+                statusMsg!!.text = "Swipe to left"
+            } else {
+                statusMsg!!.text = "Swipe to right"
+            }
+        }
+        if((deltaY_Abs >= MIN_SWIPE_DISTANCE_Y) && (deltaY_Abs<=MAX_SWIPE_DISTANCE_Y)){
+            if(deltaY>0){
+                statusMsg!!.text = "Swipe up"
+            } else {
+                statusMsg!!.text = "Swipe down"
+            }
+        }
+
+        return true
+    }
+
+    override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
+        statusMsg!!.text = "onSingleTapConfirmed"
+        return true
+    }
+
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+        statusMsg!!.text = "onDoubleTap"
+        return true
+    }
+
+    override fun onDoubleTapEvent(event: MotionEvent): Boolean {
+        statusMsg!!.text = "onDoubleTapEvent"
+        return true
+    }
+}
